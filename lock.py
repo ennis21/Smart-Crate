2198bdd91751e21b60487e6fb4446e8fcecbd34d
mikokoro1 <92599060+mikokoro1@users.noreply.github.com>
2021-10-16 01:26:30 -0500

    Update lock.py

--- lock.py
+++ lock.py
@@ -1,15 +1,9 @@
-#Import all neccessary features to code.
 import RPi.GPIO as GPIO
-from time import sleep
 
-#If code is stopped while the solenoid is active it stays active
-#This may produce a warning if the code is restarted and it finds the GPIO Pin, which it defines as non-active in next line, is still active
-#from previous time the code was run. This line prevents that warning syntax popping up which if it did would stop the code running.
 GPIO.setwarnings(False)
-#This means we will refer to the GPIO pins
-#by the number directly after the word GPIO. A good Pin Out Resource can be found here https://pinout.xyz/
+
 GPIO.setmode(GPIO.BCM)
-#This sets up the GPIO 18 pin as an output pin
+#This sets up the GPIO 23 pin as an output pin
 GPIO.setup(23, GPIO.OUT)
 
 while (True):
