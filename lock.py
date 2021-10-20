c5beff7806358685011a26731ede20ea8b783207
mikokoro1 <92599060+mikokoro1@users.noreply.github.com>
2021-10-20 18:14:22 -0500

    added led strip relay

--- lock.py
+++ lock.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
-
 GPIO.setmode(GPIO.BCM)
-#This sets up the GPIO 23 pin as an output pin
-GPIO.setup(23, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT) #lock
+GPIO.setup(24, GPIO.OUT) #LED
 
 while (True):
 
     GPIO.output(23, 1)
+    GPIO.output(24, 1)
     sleep(8)
     GPIO.output(23, 0)
+    sleep(4)
+    GPIO.output(24,0)
     sleep(1)
