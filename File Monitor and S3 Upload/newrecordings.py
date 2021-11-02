fc6bdc5ae2687db1f57a95ee17a4a202ec4d4e0f
Ennis Brown <brown.ennis8@gmail.com>
2021-11-02 13:24:02 -0500

    Add files via upload

--- File Monitor and S3 Upload/newrecordings.py
+++ File Monitor and S3 Upload/newrecordings.py
@@ -6,7 +6,7 @@ from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 import boto3 
 from botocore.exceptions import NoCredentialsError
-from secrets import access_key, secret_acess_key #AWS Access key and secret key for S3 use
+
 
 #Client creating a session to use AWS S3 functionalities
 
@@ -20,13 +20,12 @@ s3 = boto3.resource('s3')
 def upload_to_aws(local_file, bucket):
     
     #Create the s3 client
-    s3 = boto3.client('s3', aws_access_key_id=access_key,
-                      aws_secret_access_key=secret_acess_key)
+    s3 = boto3.client('s3')
 
 
     #Go through and upload all the files in the directory with exception handling
-    for file in os.listdir(path):
-        if not file.startswith('~'):
+    
+    if not file.startswith('~'):
             try:
                 
                  s3.upload_file(os.path.join(local_file, file), bucket, file)
@@ -53,7 +52,7 @@ if __name__ == "__main__":
     Function below handles the camera file path being updated
     Define the path for the recordings 
     '''
-    path = "C:/Users/Ennis/Desktop/Desktop Stuff/Fall 2021 Classes/Senior Design Project 2/Code/Video Recordings"
+    path = "C:/Users/Ennis/Desktop/Fall 2021 Classes/Senior Design Project 2/Code/Video Recordings"
     bucket = 'camerarecordings-utd1284'
     
 
@@ -75,13 +74,15 @@ if __name__ == "__main__":
             local_file: The path for the files we want to upload 
             s3_file_name: the name of our file when its sent to the bucket
             '''
-            
+            '''INFINITE LOOP'''
+            '''USE THE CAMERA TRIGGER TO UPLOAD TO THE S3 BUCKET'''
             #Make a condition where it uploads only when a new file is entered.
             #Also base the sleep time on the amount of time needed to rrecord and save the camera footage
-            uploaded = upload_to_aws(path, bucket)
+            for file in os.listdir(path):
+                 uploaded = upload_to_aws(path, bucket)
             
             #Set up the sleep time when not observing the folder 
-            time.sleep(10)
+            time.sleep(5)
     finally:
         observer.stop()
         observer.join()
